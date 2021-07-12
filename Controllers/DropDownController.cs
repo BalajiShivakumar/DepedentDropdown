bda483c41626efbd564656ccd3f75d1bfa01ff67
Balaji Shivakumar <66515822+BalajiShivakumar@users.noreply.github.com>
2021-07-12 19:53:08 +0530

    src

---
+++ Controllers/DropDownController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DependentDropdown.Models;
+using NUnit.Framework;
+
+namespace DependentDropdown.Controllers
+{
+    public class DropDownController : Controller
+    {
+        // GET: DropDown
+        
+        public ActionResult Index()
+        {
+            locationEntities sd = new locationEntities();
+            ViewBag.CountryList = new SelectList(GetCountryList(), "Cid", "Cname");
+            return View();
+        }
+
+      
+        public List<country> GetCountryList()
+        {
+            locationEntities sd = new locationEntities();
+            List<country> countries = sd.countries.ToList();
+            return countries;
+        }
+
+        
+        public ActionResult GetStateList(int Cid)
+        {
+            locationEntities sd = new locationEntities();
+            List<State> selectList = sd.States.Where(x => x.Cid == Cid).ToList();
+            ViewBag.Slist = new SelectList(selectList, "Sid", "Sname");
+            return PartialView("DisplayStates");
+        }
+
+        public ActionResult GetCityList(int Sid)
+        {
+            locationEntities sd = new locationEntities();
+            List<City> selectList = sd.Cities.Where(x => x.Sid == Sid).ToList();
+            ViewBag.Citylist = new SelectList(selectList, "Cityid", "Cityname");
+            return PartialView("DisplayCities");
+        }
+
+    }
+}
---
+++ Models/DropDownClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DependentDropdown.Models
+{
+    public class DropDownClass
+    {
+        public int Cid { get; set; }
+        public int Sid { get; set; }
+        public int Cityid { get; set; }
+
+    }
+}
